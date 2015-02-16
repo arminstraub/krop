087f53a2ec19e01fee8bc15962eb1e9111b953ec
Armin Straub <31167361+arminstraub@users.noreply.github.com>
2015-02-16 04:50:33 +0000

    Release 0.4.7

--- krop/krop.py
+++ krop/krop.py
@@ -31,6 +31,7 @@ def main():
     parser.add_argument('--whichpages', help='which pages (e.g. "1-5" or "1,3-") to include in cropped PDF (default: all)')
     parser.add_argument('--initialpage', help='which page to open initially (default: 1)')
     parser.add_argument('--autotrim', action='store_true', help='create a selection for the entire initial page minus blank margins')
+    parser.add_argument('--selections', type=str, choices=['all','evenodd','individual'], help='to which pages should selections apply')
     parser.add_argument('--no-kde', action='store_true', help='do not use KDE libraries (default: use if available)')
     parser.add_argument('--no-PyPDF2', action='store_true', help='do not use PyPDF2 instead of pyPdf (default: use PyPDF2 if available)')
 
@@ -68,6 +69,13 @@ def main():
         window.ui.editWhichPages.setText(args.whichpages)
     if args.rotate is not None:
         window.ui.comboRotation.setCurrentIndex({0:0,90:2,180:3,270:1}[args.rotate])
+    if args.selections is not None:
+        if args.selections == 'all':
+            window.ui.radioSelAll.setChecked(True)
+        elif args.selections == 'evenodd':
+            window.ui.radioSelEvenOdd.setChecked(True)
+        elif args.selections == 'individual':
+            window.ui.radioSelIndividual.setChecked(True)
     if args.initialpage is not None:
         window.ui.editCurrentPage.setText(args.initialpage)
         window.slotCurrentPageEdited(args.initialpage)
