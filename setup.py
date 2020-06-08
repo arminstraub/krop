adc984fb14e567b20622494c2da227bdff71c3c1
Armin Straub <31167361+arminstraub@users.noreply.github.com>
2020-06-08 12:11:08 -0500

    Switch to setuptools from distutils

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # -*- coding: iso-8859-1 -*-
 
 """
-Copyright (C) 2014-2018 Armin Straub, http://arminstraub.com
+Copyright (C) 2014-2020 Armin Straub, http://arminstraub.com
 """
 
 """
@@ -12,7 +12,7 @@ the Free Software Foundation; either version 3 of the License, or
 (at your option) any later version.
 """
 
-from distutils.core import setup
+from setuptools import setup
 
 
 # Automatically determine version from first line of ChangeLog file
@@ -36,9 +36,12 @@ setup(
         long_description = 'krop is a simple graphical tool to crop the pages of PDF files. It is written in Python and relies on PyQT, python-poppler-qt5 and PyPDF2 for its functionality. A unique feature of krop is its ability to automatically split pages into subpages to fit the limited screen size of devices such as eReaders. This is particularly useful, if your eReader does not support convenient scrolling.',
         keywords = 'pdf crop ereader',
         packages = ['krop'],
-        scripts = ['bin/krop'],
+        entry_points = {
+            'console_scripts': ['krop = krop.__main__:main']
+        },
+        # https://pypi.org/classifiers/
         classifiers = [
-            'Development Status :: 3 - Alpha',
+            'Development Status :: 4 - Beta',
             'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
             'Topic :: Utilities',
             'Intended Audience :: End Users/Desktop',
