24b70b1c3a7391b99bc25d955fd63df939a3f697
Armin Straub <31167361+arminstraub@users.noreply.github.com>
2023-05-20 16:10:57 -0500

    Streamline support of pypdf, PyPDF2, pikepdf via new semi abstract cropper class

--- krop/pdfcropper.py
+++ krop/pdfcropper.py
@@ -20,6 +20,7 @@ import sys
 class PdfEncryptedError(Exception):
     pass
 
+
 class AbstractPdfFile:
     """Abstract class for loading a PDF document used in a corresponding
     PdfCropper class"""
@@ -27,21 +28,6 @@ class AbstractPdfFile:
         pass
     def loadFromFile(self, filename):
         self.loadFromStream(open(filename, "rb"))
-
-class AbstractPdfCropper:
-    """Abstract class for writing a PDF documents composed of cropped pages
-    from PdfFile instances"""
-    def writeToStream(self, stream):
-        pass
-    def writeToFile(self, filename):
-        stream = open(filename, "wb")
-        self.writeToStream(stream)
-        stream.close()
-    def addPageCropped(self, pdffile, pagenumber, croplist, rotate=0):
-        pass
-    def copyDocumentRoot(self, pdffile):
-        pass
-
 
 class PyPdfFile(AbstractPdfFile):
     """Implementation of PdfFile using the new pypdf"""
@@ -74,20 +60,35 @@ class PyPdfOldFile(PyPdfFile):
     def getPage(self, nr):
         return self.reader.getPage(nr)
 
-
-class PyPdfCropper(AbstractPdfCropper):
-    """Implementation of PdfCropper using pypdf"""
+class PikePdfFile(AbstractPdfFile):
+    """Implementation of PdfFile using pikepdf"""
     def __init__(self):
-        self.output = PdfWriter()
+        self.reader = None
+    def loadFromStream(self, stream):
+        self.reader = Pdf.open(stream)
+        if self.reader.is_encrypted:
+            raise PdfEncryptedError
+    def getPage(self, nr):
+        return self.reader.pages[nr]
+
+
+class AbstractPdfCropper:
+    """Abstract class for writing a PDF documents composed of cropped pages
+    from PdfFile instances"""
     def writeToStream(self, stream):
-        # For certain large pdf files, PdfWriter.write() causes the error:
-        #   maximum recursion depth exceeded while calling a Python object
-        # This issue is present in pyPdf as well as PyPDF2 1.23
-        # We therefore temporarily increase the recursion limit.
-        old_reclimit = sys.getrecursionlimit()
-        sys.setrecursionlimit(10000)
-        self.output.write(stream)
-        sys.setrecursionlimit(old_reclimit)
+        pass
+    def writeToFile(self, filename):
+        stream = open(filename, "wb")
+        self.writeToStream(stream)
+        stream.close()
+    def addPageCropped(self, pdffile, pagenumber, croplist, rotate=0):
+        pass
+    def copyDocumentRoot(self, pdffile):
+        pass
+
+class SemiAbstractPdfCropper(AbstractPdfCropper):
+    """An extension of AbstractPdfCropper that implements the basic logic
+    breaking it down to certain functions on pages"""
     def addPageCropped(self, pdffile, pagenumber, croplist, alwaysinclude, rotate=0):
         page = pdffile.getPage(pagenumber)
         if not croplist and alwaysinclude:
@@ -101,6 +102,28 @@ class PyPdfCropper(AbstractPdfCropper):
                 if rotate != 0:
                     self.pageRotateClockwise(new_page, rotate)
                 self.doAddPage(new_page)
+    def doAddPage(self, page):
+        pass
+    def pageGetCropBox(self, page):
+        pass
+    def pageSetCropBox(self, page, box):
+        pass
+    def pageRotateClockwise(self, page, rotate):
+        pass
+
+class PyPdfCropper(SemiAbstractPdfCropper):
+    """Implementation of PdfCropper using pypdf"""
+    def __init__(self):
+        self.output = PdfWriter()
+    def writeToStream(self, stream):
+        # For certain large pdf files, PdfWriter.write() causes the error:
+        #   maximum recursion depth exceeded while calling a Python object
+        # This issue is present in pyPdf as well as PyPDF2 1.23
+        # We therefore temporarily increase the recursion limit.
+        old_reclimit = sys.getrecursionlimit()
+        sys.setrecursionlimit(10000)
+        self.output.write(stream)
+        sys.setrecursionlimit(old_reclimit)
     def doAddPage(self, page):
         self.output.add_page(page)
     def pageGetCropBox(self, page):
@@ -146,45 +169,26 @@ class PyPdfOldCropper(PyPdfCropper):
             self.output.addNamedDestinationObject(dest)
 
 
-class PikePdfFile(AbstractPdfFile):
-    """Implementation of PdfFile using pikepdf"""
-    def __init__(self):
-        self.reader = None
-    def loadFromStream(self, stream):
-        self.reader = Pdf.open(stream)
-        if self.reader.is_encrypted:
-            raise PdfEncryptedError
-
-class PikePdfCropper(AbstractPdfCropper):
+class PikePdfCropper(SemiAbstractPdfCropper):
     """Implementation of PdfCropper using pikepdf"""
     def __init__(self):
         self.output = Pdf.new()
     def writeToStream(self, stream):
         self.output.save(stream)
-    def addPageCropped(self, pdffile, pagenumber, croplist, alwaysinclude, rotate=0):
-        page = pdffile.reader.pages[pagenumber]
-        if not croplist and alwaysinclude:
-            self.output.pages.append(page)
-        for c in croplist:
-            new_page = copy.copy(page)
-            self.cropPage(new_page, c, rotate)
-            self.output.pages.append(new_page)
-    def cropPage(self, page, crop, rotate):
+    def doAddPage(self, page):
+        self.output.pages.append(page)
+    def pageGetCropBox(self, page):
         try:
             # only page.MediaBox exists in pikepdf version 1.10.3 as currently in Ubuntu 20.04
-            x0, y0, x1, y1 = page.cropbox
+            return page.cropbox
         except AttributeError:
             raise RuntimeError("Please install a more recent version of pikepdf.")
-        x0, y0, x1, y1 = float(x0), float(y0), float(x1), float(y1)
-        x0, x1 = x0+crop[0]*(x1-x0), x1-crop[2]*(x1-x0)
-        y0, y1 = y0+crop[3]*(y1-y0), y1-crop[1]*(y1-y0)
-        box = [x0, y0, x1, y1]
-        # Update the various PDF boxes.
+    def pageSetCropBox(self, page, box):
         page.mediabox = box
         page.cropbox = box
         page.trimbox = box
-        if rotate != 0:
-            page.rotate(rotate, relative=True)
+    def pageRotateClockwise(self, page, rotate):
+        page.rotate(rotate, relative=True)
     def copyDocumentRoot(self, pdffile):
         pass
 
